2eb65468879e1d4f21f91ed4d671292c84dd1b66
carquentin <quentin.carrola@ynov.com>
2024-06-17 10:39:44 +0200

    Grammar correction

--- src/main/java/com/xplit/leboncoin/util/InvalidUserInformations.java
+++ src/main/java/com/xplit/leboncoin/util/InvalidUserInformations.java
@@ -1,7 +1,7 @@
 package com.xplit.leboncoin.util;
 
 public class InvalidUserInformations extends RuntimeException {
-    public InvalidUserInformations(final String prompt) {
-        super("\nERROR : The user was skipped due to : " + prompt + '\n');
+    public InvalidUserInformations(final String message) {
+        super("\nERROR : The user was skipped due to : " + message + '\n');
     }
 }
